fbc9c27b6260e8e61e75d071f1a7a08bb6365801
unknown <melissikiss@gmail.com>
2020-12-08 13:29:15 -0800

    added chris questions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,61 @@
-const chrisQuestions [
+const chrisQuestions = [
     {
-        question: 
+        question: "What is Chris's favorite dog breed?",
+        choices: ["Beagle", "Golden Retriever", "Pug", "German Shepherd"],
+        answer: "Golden Retriever"
+    },
+
+    {
+        question: "What was Chris's favorite subject in school?",
+        choices: ["English", "Science", "Math", "PE"],
+        answer: "Math"
+    },
+
+    {
+        question: "What is Chris's favorite sport?",
+        choices: ["Soccer", "Volleyball", "Cheerleading", "Basketball"],
+        answer: "Basketball"
+    },
+
+    {
+        question: "What is Chris's favorite place to eat?",
+        choices: ["In n Out", "Popeyes", "Taco Bell", "Chick fil A"],
+        answer: "In n Out"
+    },
+
+    {
+        question: "What is Chris's signature dance move?",
+        choices: ["Stanky Leg", "The Whoa", "The Wave", "The Algebra"],
+        answer: "The Algebra",
+    },
+
+    { 
+        question: "What is Chris's go to drink at restaurants?", 
+        choices: ["Sprite", "Ice tea with no ice", "Water", "Coke"], 
+        answer: "Ice tea with no ice"
+    },
+
+    { 
+        question: "How did Chris get his name?", 
+        choices: ["Dad named him after dead relative", "Mom saw name in a baby-names book", "Randy really liked Whinnie the Pooh", "Close friend suggested it"], 
+        answer: "Randy really liked Whinnie the Pooh"
+    },
+
+    {
+        question: "Does his last name have a 'ñ' in it?", 
+        choices: ["Yes", "No", "Legally, no", "Legally yes but Chris never uses it"], 
+        answer: "Legally, no"
+    }, 
+
+    { 
+        question: "What is Chris's favorite color?", 
+        choices: ["Red", "Orange", "Pink", "Green"], 
+        answer: "Green"
+    }, 
+
+    {
+        question: "What is Chris's favorite flower?", 
+        choices: ["Tulips", "Daisies", "Roses", "Sunflowers"], 
+        answer: "Sunflowers"
     }
-]
+];
